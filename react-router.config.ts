a54e6d4f1fae0949f526de1d4f33ee94c28063dd
Steven-mca <Steven-mca@users.noreply.github.com>
2026-08-11 00:47:08 +0100

    Allow UK storefront domains

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,12 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Shopify app-proxy POSTs originate on the merchant's storefront domain
   // and are then securely forwarded to this app's host.
-  allowedActionOrigins: ["admin.shopify.com", "*.myshopify.com"],
+  allowedActionOrigins: [
+    "admin.shopify.com",
+    "*.myshopify.com",
+    "performancebodykits.com",
+    "www.performancebodykits.com",
+    "performancebodykits.co.uk",
+    "www.performancebodykits.co.uk",
+  ],
 } satisfies Config;
